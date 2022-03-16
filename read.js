12156ed21dfc3a3dabb1c57e1c1ef71fa1c24b19
Prabhakar Kumar <kumarprabhakar2121@gmail.com>
2022-03-16 18:53:51 +0530

    initial commit

---
+++ read.js
@@ -0,0 +1,19 @@
+// Requiring the module
+const reader = require("xlsx");
+
+// Reading our test file
+const file = reader.readFile("./test1.xlsx");
+console.log(file);
+let data = [];
+
+const sheets = file.SheetNames;
+
+for (let i = 0; i < sheets.length; i++) {
+  const temp = reader.utils.sheet_to_json(file.Sheets[file.SheetNames[i]]);
+  temp.forEach((res) => {
+    data.push(res);
+  });
+}
+
+// Printing data
+console.log(data);
